c866f84076425551f4bf08ae3fe699bfe0c016a8
Luigi Porrega <148718173+LuigiPorrega@users.noreply.github.com>
2025-09-30 12:34:01 +0200

    cambio environment a produccion

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseURL: "http://65.108.85.99:8000/api",
+  baseURL: "https://apisolar.luigicodes.com/api",
   meteoApiKey : '0effb1e08931d718c573e5787ab664e6',
   //meteoApiUrl : 'https://api.openweathermap.org/data/2.5/weather',
   meteoApiUrl: '/api-clima/data/2.5/weather'
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseURL: "https:/www.api.luigicodes.com",
+  baseURL: "https://apisolar.luigicodes.com/api",
   meteoApiKey : '0effb1e08931d718c573e5787ab664e6',
   meteoApiUrl: '/api-clima/data/2.5/weather'
   //meteoApiUrl : 'https://api.openweathermap.org/data/2.5/weather',
