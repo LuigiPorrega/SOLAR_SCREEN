f53675877ea710c0a4d94a59fc54434331abab62
Luigi Porrega <148718173+LuigiPorrega@users.noreply.github.com>
2025-09-30 11:48:36 +0200

    change environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseURL: "http://65.108.85.99:8000/api",
+  baseURL: "https:/www.api.luigicodes.com",
   meteoApiKey : '0effb1e08931d718c573e5787ab664e6',
   meteoApiUrl: '/api-clima/data/2.5/weather'
   //meteoApiUrl : 'https://api.openweathermap.org/data/2.5/weather',
